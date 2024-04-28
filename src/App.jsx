9b64015b0a69b213b90b744dadc8bc7e26d85969
Parth Sharma <parthsharma2880@gmail.com>
2024-04-28 17:56:28 +0530

    Added: react & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is App component</h1>
+    </>
+  )
+}
+
+export default App
