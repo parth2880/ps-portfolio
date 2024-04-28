ad3d017cbd636661f6beba3da21392d4d34ccdd3
Parth Sharma <parthsharma2880@gmail.com>
2024-04-28 18:55:05 +0530

    Added: Dummy Header & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>This is App component</h1>
+      <div className="max-w-screen-xl mx-auto">
+        <Header />
+        <Footer />
+      </div>
     </>
   )
 }
